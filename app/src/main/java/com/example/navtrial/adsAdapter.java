ac342de107833f8f1ec11c170666e95b6ded9828
unknown <bereketheramo25@gmail.com>
2019-12-17 11:01:47 +0300

    Image loading is completed

--- app/src/main/java/com/example/navtrial/adsAdapter.java
+++ app/src/main/java/com/example/navtrial/adsAdapter.java
@@ -1,21 +1,31 @@
 package com.example.navtrial;
 
 import android.content.Context;
+
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
+
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import com.example.navtrial.data.event;
+import com.squareup.picasso.Picasso;
+
 
 import java.util.List;
 
+
+
 public class adsAdapter extends RecyclerView.Adapter<adsAdapter.ViewHolder> {
-    private List<event> ads;
+
+    private  List<event> ads;
     private Context context;
     private List<event> mevents;
 
@@ -27,7 +37,7 @@ public class adsAdapter extends RecyclerView.Adapter<adsAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        View view = View.inflate(parent.getContext(),R.layout.ads_recycler_view_list_item,null);
+
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ads_recycler_view_list_item,parent,false);
 
 
@@ -35,28 +45,28 @@ public class adsAdapter extends RecyclerView.Adapter<adsAdapter.ViewHolder> {
 
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    public void onBindViewHolder(  @NonNull  final ViewHolder holder, int position) {
+
+
 
      if(ads!=null){
-         event ad = ads.get(position);
+         final event ad = ads.get(position);
+
+
          holder.ads_name.setText(ad.getName());
+        Picasso.get().load("http://10.0.2.2:3000/api/containers/ads/download/" + ad.getImage()).into(holder.im);
+
          holder.ads_organizer.setText(ad.getOrganizer());
          holder.ads_location.setText(ad.getLocation());
          holder.ads_date.setText(ad.getDate());
 
 
      }
-     else{
-
-     }
-//        Ads ad = ads.get(position);
-//        holder.ads_name.setText(ad.getAds_name());
-//        holder.ads_organizer.setText(ad.getAds_organizer());
-//        holder.ads_location.setText(ad.getAds_location());
-//        holder.ads_date.setText(ad.getads_date());
 
     }
+
+
 
     public void setAds(List<event> events){
      mevents = events;
@@ -72,13 +82,14 @@ public class adsAdapter extends RecyclerView.Adapter<adsAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         public TextView ads_name,ads_organizer,ads_date,ads_category,ads_location;
+        public ImageView im;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ads_name = itemView.findViewById(R.id.ads_name_text_view_card_view);
             ads_organizer = itemView.findViewById(R.id.ads_organizer_text_view);
             ads_location = itemView.findViewById(R.id.ads_location_text_view);
             ads_date = itemView.findViewById(R.id.ads_date_text_view);
-
+            im = itemView.findViewById(R.id.image_view);
         }
     }
 }
--- app/src/main/java/com/example/navtrial/webService/GetAdsService.java
+++ app/src/main/java/com/example/navtrial/webService/GetAdsService.java
@@ -4,10 +4,13 @@ import com.example.navtrial.data.event;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Url;
 
 public interface GetAdsService {
-    @GET("/ads")
+    @GET("ads")
     Call<List<event>> getAllAds();
+
 }
