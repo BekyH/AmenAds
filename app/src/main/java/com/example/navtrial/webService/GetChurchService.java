e58b94b82498a685db09eac68fd63d09014d9570
unknown <bereketheramo25@gmail.com>
2019-12-23 15:46:53 +0300

    getting church from api

---
+++ app/src/main/java/com/example/navtrial/webService/GetChurchService.java
@@ -0,0 +1,13 @@
+package com.example.navtrial.webService;
+
+import com.example.navtrial.data.church;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetChurchService {
+    @GET("churches")
+    Call<List<church>> getChruches();
+}
