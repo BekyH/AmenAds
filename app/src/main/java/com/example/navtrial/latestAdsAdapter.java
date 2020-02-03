558b9461f8efa27d465b586a0561ccd0f1225681
unknown <bereketheramo25@gmail.com>
2020-02-02 21:17:02 -0800

    latest tab added

---
+++ app/src/main/java/com/example/navtrial/latestAdsAdapter.java
@@ -0,0 +1,76 @@
+package com.example.navtrial;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.navtrial.data.event;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class latestAdsAdapter extends RecyclerView.Adapter<latestAdsAdapter.ViewHolder> {
+
+    private List<event> ads;
+    private Context context;
+    private List<event> mevents;
+
+    public latestAdsAdapter(Context context,List<event> ads){
+        this.context = context;
+        this.ads = ads;
+        LayoutInflater.from(context);
+
+
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.latest_ads_recyclerview,parent,false);
+
+
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if(ads!=null){
+            final event ad = ads.get(position);
+
+
+            holder.ads_name.setText(ad.getName());
+            Picasso.get().load("http://10.0.2.2:3000/api/containers/ads/download/" + ad.getImage()).into(holder.im);
+            // Picasso.get().load("http://10.5.225.89:3000/api/containers/ads/download/" + ad.getImage()).into(holder.im);
+            holder.ads_organizer.setText(ad.getOrganizer());
+            holder.ads_location.setText(ad.getLocation());
+            holder.ads_date.setText(ad.getDate());
+
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ads.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView ads_name,ads_organizer,ads_date,ads_category,ads_location;
+        public ImageView im;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ads_name = itemView.findViewById(R.id.ads_name_text_view_card_view);
+            ads_organizer = itemView.findViewById(R.id.ads_organizer_text_view);
+            ads_location = itemView.findViewById(R.id.ads_location_text_view);
+            ads_date = itemView.findViewById(R.id.ads_date_text_view);
+            im = itemView.findViewById(R.id.image_view);
+        }
+    }
+}
--- app/src/main/java/com/example/navtrial/latesttabfragment.java
+++ app/src/main/java/com/example/navtrial/latesttabfragment.java
@@ -1,19 +1,104 @@
 package com.example.navtrial;
 
+import android.app.ProgressDialog;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.navtrial.data.event;
+import com.example.navtrial.webService.GetAdsService;
+import com.example.navtrial.webService.ServiceBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class latesttabfragment extends Fragment {
+    private RecyclerView latestadsRecyclerView;
+    ProgressDialog progressDialog;
+    private RecyclerView.Adapter latestadsRecyclerAdapter;
+    ArrayList<event> latestads;
+    View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.latesttabfragment,container,false);
+        view =  inflater.inflate(R.layout.latesttabfragment,container,false);
+
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+        GetAdsService ads_service = ServiceBuilder.getRetrofitInstance().create(GetAdsService.class);
+        Call<List<event>> call = ads_service.getAllAds();
+        call.enqueue(new Callback<List<event>>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onResponse(Call<List<event>> call, retrofit2.Response<List<event>> response) {
+                progressDialog.dismiss();
+
+                if(response.isSuccessful()){
+                    //  Toast.makeText(getContext(),response.body().toString(),Toast.LENGTH_SHORT).show();
+                    latesteventlist(response.body());
+
+                }
+                else {
+                    Toast.makeText(getContext(),"response is not successfull",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<event>> call, Throwable t) {
+                progressDialog.dismiss();
+                if (t instanceof IOException) {
+
+                    Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                    // logging probably not necessary
+                }
+                else {
+                    Toast.makeText(getContext(), "conversion issue! big problems", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        return view;
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void latesteventlist(List<event> eventList){
+        String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        latestads = new ArrayList<>();
+        for(int i = 0;i<eventList.size();i++) {
+            if (!date.equals(eventList.get(i).getDate())) {
+                latestads.add(eventList.get(i));
+
+            }
+        }
+        latestadsRecyclerView = view.findViewById(R.id.latest_tab_recycler_view);
+        latestadsRecyclerAdapter = new todayAdsAdapter(getContext(),latestads);
+        latestadsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        latestadsRecyclerView.setAdapter(latestadsRecyclerAdapter);
+
+
+
+
+
 
     }
 }
