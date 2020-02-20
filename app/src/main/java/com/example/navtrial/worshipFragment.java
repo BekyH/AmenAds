e801e25128e4ceb9ee7767e2ffa7f3727a462b0e
unknown <bereketheramo25@gmail.com>
2020-02-19 16:08:20 -0800

    changed types

--- app/src/main/java/com/example/navtrial/worshipFragment.java
+++ app/src/main/java/com/example/navtrial/worshipFragment.java
@@ -1,4 +1,94 @@
 package com.example.navtrial;
 
-public class worshipFragment {
+import android.app.ProgressDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.navtrial.data.event;
+import com.example.navtrial.webService.GetAdsService;
+import com.example.navtrial.webService.ServiceBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class worshipFragment extends Fragment {
+    private RecyclerView worshipadsRecyclerView;
+    ProgressDialog progressDialog;
+    private RecyclerView.Adapter worshipadsRecyclerAdapter;
+    ArrayList<event> worshipads;
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.worship_fragment,container,false);
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+        GetAdsService ads_service = ServiceBuilder.getRetrofitInstance().create(GetAdsService.class);
+        Call<List<event>> call = ads_service.getAllAds();
+        call.enqueue(new Callback<List<event>>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onResponse(Call<List<event>> call, retrofit2.Response<List<event>> response) {
+                progressDialog.dismiss();
+
+                if(response.isSuccessful()){
+                    //  Toast.makeText(getContext(),response.body().toString(),Toast.LENGTH_SHORT).show();
+                    getConferenceAds(response.body());
+                }
+                else {
+                    Toast.makeText(getContext(),"response is not successfull",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<event>> call, Throwable t) {
+                progressDialog.dismiss();
+                if (t instanceof IOException) {
+
+                    Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                    // logging probably not necessary
+                }
+                else {
+                    Toast.makeText(getContext(), "conversion issue! big problems", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+
+        return view;
+    }
+
+    public void getConferenceAds(List<event> allads){
+        worshipads = new ArrayList<>();
+        for(int i = 0;i<allads.size();i++){
+            if(allads.get(i).getCategory().equals("worship")){
+                worshipads.add(allads.get(i));
+
+            }
+        }
+        worshipadsRecyclerView = view.findViewById(R.id.conference_recycler_view);
+        worshipadsRecyclerAdapter = new conferenceAdapter(getContext(),worshipads);
+        worshipadsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        worshipadsRecyclerView.setAdapter(worshipadsRecyclerAdapter);
+
+    }
 }
