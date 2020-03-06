efb6033aab3eb667355063910b685306e89fd752
unknown <bereketheramo25@gmail.com>
2020-03-05 23:36:17 -0800

    church detail adapter completed

--- app/src/main/java/com/example/navtrial/churchDetailAdapter.java
+++ app/src/main/java/com/example/navtrial/churchDetailAdapter.java
@@ -1,33 +1,77 @@
 package com.example.navtrial;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class churchDetailAdapter extends RecyclerView.Adapter<churchDetailAdapter.ViewHolder> {
+import com.example.navtrial.data.church;
 
+import java.util.List;
+
+public class churchDetailAdapter extends RecyclerView.Adapter<churchDetailAdapter.ViewHolder> {
+private Context context;
+private List<church> mchurches;
+
+            public churchDetailAdapter(Context context,List<church> mchurches){
+                this.context = context;
+                this.mchurches = mchurches;
+                LayoutInflater.from(context);
+
+            }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.church_detail_recycler_item,parent,false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    if(mchurches!=null){
+        church branches = mchurches.get(position);
+        holder.branchName.setText(branches.getChurchName());
+        holder.mainChurchName.setText(branches.getMainchurch());
+        holder.churchLocation.setText(branches.getLocation());
+        holder.churchProgram.setText(branches.getProgram());
+        holder.churchDescritpion.setText(branches.getDescription());
+        holder.churchBankAccount.setText(branches.getBankAccount());
 
+    }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+                if(mchurches!=null){
+                    return mchurches.size();
+
+                }
+                else{
+                    return 0;
+                }
+
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-
+        public TextView branchName;
+        public TextView mainChurchName;
+        public TextView churchLocation;
+        public TextView churchProgram;
+        public TextView churchDescritpion;
+        public TextView churchBankAccount;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            branchName = itemView.findViewById(R.id.branch_name_text_view);
+            mainChurchName = itemView.findViewById(R.id.main_church_name_text_view);
+            churchLocation = itemView.findViewById(R.id.church_location_text_view);
+            churchProgram = itemView.findViewById(R.id.church_program_text_view);
+            churchDescritpion = itemView.findViewById(R.id.church_description_text_view);
+            churchBankAccount = itemView.findViewById(R.id.church_bank_account_text_view);
         }
     }
 }
